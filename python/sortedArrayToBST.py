85b07c926ca7ec1c1f041acae0484e786828e4e1
Xuyuan Pang <xuyuanp@gmail.com>
2014-06-08 17:20:20 +0800

    sorted link to BST

--- python/sortedArrayToBST.py
+++ python/sortedArrayToBST.py
@@ -6,6 +6,11 @@ class TreeNode:
         self.left = None
         self.right = None
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
     def sortedArrayToBST(self, num):
@@ -15,4 +20,26 @@ class Solution:
         root = TreeNode(num[mid])
         root.left = self.sortedArrayToBST(num[:mid])
         root.right = self.sortedArrayToBST(num[mid + 1:])
+        return root
+
+    def sortedListToBST(self, head):
+        array = []
+        length = 0
+        node = head
+        self.head = head
+        while node:
+            length += 1
+            node = node.next
+        return self.__sortedListToBST(0, length - 1)
+
+    def __sortedListToBST(self, start, end):
+        if start > end:
+            return None
+        mid = (end + start) / 2
+        left = self.__sortedListToBST(start, mid - 1)
+        root = TreeNode(self.head.val)
+        self.head = self.head.next
+        right = self.__sortedListToBST(mid + 1, end)
+        root.left = left
+        root.right = right
         return root
